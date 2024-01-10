2e05ba88ecc99de02fe6a048e42453c289311d5a
Aizhan Nazirbaeva <aizhannazirbaeva@Aizhans-Air.attlocal.net>
2024-01-09 23:48:44 -0600

    created version 0.3

--- src/test/java/Day3.java
+++ src/test/java/Day3.java
@@ -2,5 +2,6 @@ public class Day3 {
     public static void main(String[] args) {
         System.out.println("This is version 0.1");
         System.out.println("Added more changes. This is version 0.2");
+        System.out.println("Added a few  more changes. This is version 0.3");
     }
 }
